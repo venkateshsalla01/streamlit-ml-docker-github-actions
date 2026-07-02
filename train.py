6c270fa2b6813beb631ad19d3fa8f18e3970eb57
Venkatesh Salla <58113941+venkateshsalla01@users.noreply.github.com>
2026-07-02 09:00:05 +0530

    Update train.py

---
+++ train.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.datasets import fetch_california_housing
+from sklearn.ensemble import RandomForestRegressor
+import joblib
+
+housing = fetch_california_housing(as_frame=True)
+
+X= housing.data
+y = housing.target
+
+model = RandomForestRegressor()
+
+model.fit(X,y)
+
+joblib.dump(model,"model/model.pkl")
